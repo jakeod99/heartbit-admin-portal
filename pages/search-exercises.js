4411d3445077228961c5e44ea7e981178b987ded
Jake O'Donnell <jakeod99@gmail.com>
2022-01-25 16:57:40 -0500

    broke search into user and exercise

---
+++ pages/search-exercises.js
@@ -0,0 +1,9 @@
+const SearchExercises = () => {
+  return ( 
+    <div>
+      <h1>Search Exercises</h1>
+      <p>Page for querying research data - strictly idempotent</p>
+    </div> );
+}
+ 
+export default SearchExercises;
---
+++ pages/search-users.js
@@ -0,0 +1,10 @@
+const SearchUsers = () => {
+  return ( 
+    <div>
+      <h1>Search Users</h1>
+      <p>Page for querying user information - strictly idempotent</p>
+    </div>
+   );
+}
+ 
+export default SearchUsers;
