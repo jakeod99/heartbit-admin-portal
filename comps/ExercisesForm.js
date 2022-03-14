f55732775747377b9a5b1d812e2ec67bf1af76ae
Jake O'Donnell <jakeod99@gmail.com>
2022-03-14 18:45:43 -0400

    first query column

--- comps/ExercisesForm.js
+++ comps/ExercisesForm.js
@@ -28,44 +28,84 @@ class ExercisesForm extends React.Component {
       <Container className="ef-container">
         <Form className="ef">
           <Row>
-            <Col>
+            <Col md={5}>
               <Row>
                 <Col>
-                  <p className="ef-age-range-label">Age Range</p>
-                  <Row>
-                    {/* <Col md={5} className="ef-age-range-young-col"> */}
+                  <div className="ef-age-range-container">
+                    <p className="ef-age-range-label">Age Range</p>
+                    <Row>
                       <FormControl 
                         type="number" 
                         placeholder="youngest" 
-                        className="ef-age-range-young-col" />
-                    {/* </Col> */}
-                    {/* <Col md={2} className="ef-age-range-dash">-</Col> */}
-                    {/* <Col md={5} className="ef-age-range-old-col"> */}
+                        className="ef-age-range-young" 
+                      />
                       <div className="ef-age-range-dash">-</div>
                       <FormControl 
                         type="number" 
                         placeholder="oldest"
-                        className="ef-age-range-old-col" />
-                    {/* </Col> */}
-                  </Row>
+                        className="ef-age-range-old" 
+                      />
+                    </Row>
+                  </div>
+                  <div className="ef-email-container">
+                    <p className="ef-email-label">Email</p>
+                    <FormControl 
+                        placeholder="john@example.com" 
+                        className="ef-email" 
+                      />
+                  </div>
                 </Col>
-                <div className="ef-weight-range-container"></div>
+                <Col>
+                  <div className="ef-weight-range-container">
+                    <p className="ef-weight-range-label">Weight Range</p>
+                    <Row>
+                      <FormControl 
+                        type="number" 
+                        placeholder="lightest" 
+                        className="ef-weight-range-light" 
+                      />
+                      <div className="ef-weight-range-dash">-</div>
+                      <FormControl 
+                        type="number" 
+                        placeholder="heaviest"
+                        className="ef-weight-range-heavy" 
+                      />
+                    </Row>
+                  </div>
+                  <div className="ef-group-container">
+                    <p className="ef-group-label">Group</p>
+                    <FormControl 
+                        placeholder="TODO" 
+                        className="ef-group" 
+                        disabled={true}
+                      />
+                  </div>
+                </Col>
               </Row>
-              <Row>
-                <div className="ef-email-container"></div>
-                <div className="ef-group-container"></div>
-              </Row>
-              <div className="ef-date-range-container"></div>
+              <div className="ef-collection-range-container">
+                <p className="ef-collection-range-label">Collection Date Range</p>
+                <Row>
+                  <FormControl 
+                    type="date" 
+                    className="ef-collection-range-earliest" 
+                  />
+                  <div className="ef-collection-range-dash">-</div>
+                  <FormControl 
+                    type="date" 
+                    className="ef-collection-range-latest" 
+                  />
+                </Row>
+              </div>
             </Col>
-            <Col>
+            <Col md={2}>
               <div className="ef-sex-container"></div>
               <div className="ef-gender-container"></div>
             </Col>
-            <Col>
+            <Col md={2}>
               <div className="ef-tobacco-container"></div>
               <div className="ef-vape-container"></div>
             </Col>
-            <Col>
+            <Col md={3}>
               <div className="ef-race-container"></div>
               <div className="ef-clean-container"></div>
               <Button className="ef-submit" variant="info" onClick={() => this.props.getExercises({
--- comps/ExercisesTable.js
+++ comps/ExercisesTable.js
@@ -8,7 +8,7 @@ const ExercisesTable = ({ data }) => {
           <thead className="et-head">
             <tr>
               <th className="et-head-date-collected">Collected</th>
-              {/* <th>Group ID</th> */}
+              {/* <th className="et-head-group">Group ID</th> */}
               <th className="et-head-email">Email</th>
               <th className="et-head-dob">Date of Birth</th> 
               <th className="et-head-sex">Sex</th>
@@ -24,20 +24,20 @@ const ExercisesTable = ({ data }) => {
           <tbody className="et-body">
             {data?.exercises?.map(exercise => (
               <tr>
-                <td className="et-head-date-collected">{exercise.dateCollected.split("T")[0]}</td>
-                {/* <th>Group ID</th> */}
-                <td className="et-head-email">{exercise.user.email}</td>
-                <td className="et-head-dob">{exercise.user.dob.split("T")[0]}</td>
-                <td className="et-head-sex">{exercise.user.sex}</td>
-                <td className="et-head-gender">{exercise.user.gender.replace(/_/g, " ").replace("GENDER ", "")}</td>
-                <td className="et-head-race">{exercise.user.race.replace(/_/g, " ")}</td>
-                <td className="et-head-weight">{exercise.user.weight}lbs</td>
-                <td className="et-head-bpm-in">{exercise.bpmIn}bpm</td>
-                <td className="et-head-bpm-out">{exercise.bpmOut}bpm</td>
-                <td className="et-head-data">
+                <td className="et-body-date-collected">{exercise.dateCollected.split("T")[0]}</td>
+                {/* <td className="et-body-group">Group ID</td> */}
+                <td className="et-body-email">{exercise.user.email}</td>
+                <td className="et-body-dob">{exercise.user.dob.split("T")[0]}</td>
+                <td className="et-body-sex">{exercise.user.sex}</td>
+                <td className="et-body-gender">{exercise.user.gender.replace(/_/g, " ").replace("GENDER ", "")}</td>
+                <td className="et-body-race">{exercise.user.race.replace(/_/g, " ")}</td>
+                <td className="et-body-weight">{exercise.user.weight}lbs</td>
+                <td className="et-body-bpm-in">{exercise.bpmIn}bpm</td>
+                <td className="et-body-bpm-out">{exercise.bpmOut}bpm</td>
+                <td className="et-body-data">
                   <Button variant="primary">TODO</Button>
                 </td>
-                <td className="et-head-clean">{exercise.status.replace("DIRTY", "RAW")}</td>
+                <td className="et-body-clean">{exercise.status.replace("DIRTY", "RAW")}</td>
               </tr>
             ))}
           </tbody>
